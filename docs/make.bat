b54db2c7f04503a141d53fab79722bb0a099dfa2
Byron <53088136+byronbytes@users.noreply.github.com>
2021-10-15 13:43:52 -0400

    tiny change to error message

--- docs/make.bat
+++ docs/make.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+@echo off
 
 pushd %~dp0
 
@@ -15,13 +15,13 @@ if "%1" == "" goto help
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
+	echo. The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo. installed, then set the SPHINXBUILD environment variable to point
+	echo. to the full path of the 'sphinx-build' executable. Alternatively you
+	echo. may add the Sphinx directory to PATH.
 	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
+	echo. If you don't have Sphinx installed, download it from
+	echo. https://www.sphinx-doc.org/
 	exit /b 1
 )
 
